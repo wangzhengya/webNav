49e5b072f2e041e271c4617d815677812cebc8af
zhengyaing <wang_zhengya@163.com>
2020-01-01 19:36:55 +0800

    代码上传heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 connectDB();
@@ -7,15 +8,19 @@ connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to zhengyang‘s website navigation' })
-);
-
 //Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/links', require('./routes/links'));
 app.use('/api/categories', require('./routes/categories'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`服务器运行在端口：${PORT}`));
